e24dc8f6d24d0cdccc31021ce0b222f950030720
yufanpin <3281107880@qq.com>
2025-07-20 00:30:08 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -60,6 +60,8 @@ UPDATE_PACKAGE "OpenAppFilter" "destan19/OpenAppFilter" "master"
 UPDATE_PACKAGE "luci-app-advancedplus" "sirpdboy/luci-app-advancedplus" "main"
 # 添加设置向导
 UPDATE_PACKAGE "luci-app-wizard" "kiddin9/luci-app-wizard" "main"
+#添加看门狗
+UPDATE_PACKAGE "luci-app-watchdog" "sirpdboy/luci-app-watchdog" "main"
 
 UPDATE_PACKAGE "nikki" "nikkinikki-org/OpenWrt-nikki" "main"
 UPDATE_PACKAGE "openclash" "vernesong/OpenClash" "dev" "pkg"
