d35202c4ce29cc86897440737c7f036b1d670300
yufanpin <3281107880@qq.com>
2026-06-09 10:50:06 +0800

    添加podman运行时、清理easymesh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -86,7 +86,6 @@ UPDATE_PACKAGE "luci-app-podman" "Zerogiven-OpenWRT-Packages/luci-app-podman" "m
 
 UPDATE_PACKAGE "OpenAppFilter" "destan19/OpenAppFilter" "master" "" "luci-app-oaf oaf open-app-filter"
 UPDATE_PACKAGE "luci-app-gecoosac" "laipeng668/luci-app-gecoosac" "main" "" "gecoosac"
-UPDATE_PACKAGE "luci-app-easymesh" "torguardvpn/luci-app-easymesh" "main"
 UPDATE_PACKAGE "luci-app-change-mac" "muink/luci-app-change-mac" "main"
 
 #更新软件包版本
